5920ebaa41113192dbc5d0f556811f6b0603f90e
ahs.1995 <ashik.srkr95@gmail.com>
2023-09-09 12:38:56 +0530

    Added statistics fetutre

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,22 @@
-export default function Stats() {
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 📃</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const perchantage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>You have x items on your list, and you already packed x(x% )</em>
+      <em>
+        {perchantage === 100
+          ? "You are all packed and ready to go! ✈"
+          : `You have ${numItems} items on your list, and you already packed 
+        ${numPacked} (${perchantage}% )`}
+      </em>
     </footer>
   );
 }
